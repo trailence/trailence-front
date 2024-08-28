2602f079b6586daeed230e074e178f9f02a9d109
Guillaume Le Cousin <guillaume@lecousin.net>
2024-08-28 04:11:38 +0200

    open trail and collection as link to allow open in new tab

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Injector, Input } from '@angular/core';
 import { IonHeader, IonToolbar, IonButtons, IonIcon, IonMenuButton, IonButton, IonPopover, IonContent, IonBadge } from '@ionic/angular/standalone';
 import { HeaderUserMenuComponent } from '../header-user-menu/header-user-menu.component';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MenuItem } from 'src/app/utils/menu-item';
-import { IdGenerator } from 'src/app/utils/component-utils';
+import { AbstractComponent, IdGenerator } from 'src/app/utils/component-utils';
 import { MenuContentComponent } from '../menu-content/menu-content.component';
 import { UpdateService } from 'src/app/services/update/update.service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -25,7 +26,7 @@ import { UpdateService } from 'src/app/services/update/update.service';
     MenuContentComponent,
   ]
 })
-export class HeaderComponent {
+export class HeaderComponent extends AbstractComponent {
 
   @Input() title = '';
   @Input() backUrl?: string;
@@ -34,13 +35,27 @@ export class HeaderComponent {
   id = IdGenerator.generateId();
 
   constructor(
+    injector: Injector,
     public auth: AuthService,
     private router: Router,
     public update: UpdateService,
-  ) {}
+  ) {
+    super(injector);
+  }
 
   back(): void {
     this.router.navigateByUrl(this.backUrl!);
+  }
+
+  protected override getComponentState() {
+    return this.title;
+  }
+
+  protected override onComponentStateChanged(previousState: any, newState: any): void {
+    this.byStateAndVisible.subscribe(of(true), () => {
+      const title = document.getElementsByTagName('head')[0].getElementsByTagName('title')[0];
+      title.innerText = this.title + ' - Trailence';
+    });
   }
 
 }
