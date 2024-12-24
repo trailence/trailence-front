f5bf1ade5d0ab63ee194be2b0ddd1dcacb36a8e8
Guillaume Le Cousin <guillaume@lecousin.net>
2024-12-24 08:28:02 +0100

    v0.11.0

--- src/app/services/i18n/i18n.service.ts
+++ src/app/services/i18n/i18n.service.ts
@@ -7,7 +7,7 @@ import { StringUtils } from 'src/app/utils/string-utils';
 import { AssetsService } from '../assets/assets.service';
 import { Console } from 'src/app/utils/console';
 
-const TEXTS_VERSION = '7';
+const TEXTS_VERSION = '8';
 
 @Injectable({
   providedIn: 'root'
--- src/app/trailence-version.ts
+++ src/app/trailence-version.ts
@@ -1,2 +1,2 @@
-export const trailenceAppVersionName="0.10.1";
-export const trailenceAppVersionCode=1001;
+export const trailenceAppVersionName="0.11.0";
+export const trailenceAppVersionCode=1100;
