1b1e6d68fd3e85ad484aa7381990fd592462bab2
Guillaume Le Cousin <guillaume@lecousin.net>
2024-12-24 08:59:11 +0100

    v0.11.1

--- src/app/trailence-version.ts
+++ src/app/trailence-version.ts
@@ -1,2 +1,2 @@
-export const trailenceAppVersionName="0.11.0";
-export const trailenceAppVersionCode=1100;
+export const trailenceAppVersionName="0.11.1";
+export const trailenceAppVersionCode=1101;
