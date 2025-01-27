babc2d3ae3d3c685dd504a67e83967354b144e15
Guillaume Le Cousin <guillaume@lecousin.net>
2025-01-27 05:28:40 +0100

    allow to use Cloudflare turnstile instead of Google reCaptcha, with compliance with GDPR

--- src/app/services/captcha/captcha.service.ts
+++ src/app/services/captcha/captcha.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, combineLatest, filter, first, map, Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, filter, first, map, Observable, of } from 'rxjs';
 import { HttpService } from '../http/http.service';
 import { environment } from 'src/environments/environment';
 import { IdGenerator } from 'src/app/utils/component-utils';
 import { Console } from 'src/app/utils/console';
+
+interface ConfigFromServer {
+  provider: string;
+  clientKey: string;
+}
+
+interface CaptchaConfig {
+  enabled: boolean;
+  scriptUrl: string;
+  render: (elementId: string, onsuccess: (token: string) => void, onexpired: () => void, onerror: (error: any) => void) => void;
+}
 
 @Injectable({providedIn: 'root'})
 export class CaptchaService {
@@ -11,7 +22,7 @@ export class CaptchaService {
   private readonly jsLoaded = new BehaviorSubject<boolean>(false);
   private jsLoading = false;
   private node?: HTMLScriptElement;
-  private readonly key = new BehaviorSubject<string | undefined>(undefined);
+  private readonly config = new BehaviorSubject<CaptchaConfig | undefined>(undefined);
 
   constructor(private readonly http: HttpService) { }
 
@@ -23,17 +34,8 @@ export class CaptchaService {
     container.id = elementId;
     element.appendChild(container);
     this.prepare();
-    this.ready$().subscribe(() => {
-      if (this.key.value?.length === 0) {
-        onsuccess('disabled');
-        return;
-      }
-      (window as any).grecaptcha.render(elementId, {
-        'sitekey' : this.key.value,
-        'callback': onsuccess,
-        'expired-callback': onexpired,
-        'error-callback': onerror
-      });
+    this.ready$().subscribe(config => {
+      config.render(elementId, onsuccess, onexpired, onerror);
     });
   }
 
@@ -42,17 +44,16 @@ export class CaptchaService {
     if (element) while (element.children.length > 0) element.removeChild(element.children.item(0)!);
   }
 
-  private ready$(): Observable<boolean> {
-    return combineLatest([this.jsLoaded, this.key]).pipe(
-      filter(([js, key]) => !!js && key !== undefined),
+  private ready$(): Observable<CaptchaConfig> {
+    return combineLatest([this.jsLoaded, this.config]).pipe(
+      filter(([js, config]) => !!js && config !== undefined),
       first(),
-      map(() => true)
+      map(([js, config]) => config!)
     );
   }
 
   public prepare(): void {
-    this.ensureKey();
-    this.ensureJS();
+    this.ensureConfig().subscribe(() => this.ensureJS());
   }
 
   private ensureJS(): void {
@@ -63,19 +64,19 @@ export class CaptchaService {
   }
 
   private loadJS(): void {
-    this.key.pipe(
+    this.config.pipe(
       filter(k => k !== undefined),
       first()
-    ).subscribe(() => {
-      if (this.key.value?.length === 0) {
+    ).subscribe(c => {
+      if (!c.enabled) {
         this.jsLoaded.next(true);
         return;
       }
-      (window as any).grecaptcha_onloadCallback = () => {
+      (window as any).captcha_onloadCallback = () => {
         this.jsLoaded.next(true);
       };
       this.node = document.createElement('script');
-      this.node.src = 'https://www.google.com/recaptcha/api.js?onload=grecaptcha_onloadCallback&render=explicit';
+      this.node.src = c.scriptUrl;
       this.node.type = 'text/javascript';
       this.node.async = true;
       this.node.defer = true;
@@ -86,13 +87,57 @@ export class CaptchaService {
     });
   }
 
-  private ensureKey(): void {
-    if (this.key.value !== undefined) return;
-    this.loadKey();
+  private ensureConfig(): Observable<CaptchaConfig> {
+    if (this.config.value !== undefined) return of(this.config.value);
+    return this.loadConfig();
   }
 
-  private loadKey(): void {
-    this.http.getString(environment.apiBaseUrl + '/auth/v1/captcha').subscribe(key => {Console.info('Captcha key: <' + key + '>'); this.key.next(key); });
+  private loadConfig(): Observable<CaptchaConfig> {
+    return this.http.get<ConfigFromServer>(environment.apiBaseUrl + '/auth/v1/captcha')
+    .pipe(
+      map(config => {
+        Console.info('Captcha config', config);
+        let c: CaptchaConfig;
+        if (config.provider === 'recaptcha' && config.clientKey.length > 0) {
+          c = {
+            enabled: true,
+            scriptUrl: 'https://www.google.com/recaptcha/api.js?onload=captcha_onloadCallback&render=explicit',
+            render: (elementId, onsuccess, onexpired, onerror) => {
+              (window as any).grecaptcha.render(elementId, {
+                'sitekey' : config.clientKey,
+                'callback': onsuccess,
+                'expired-callback': onexpired,
+                'error-callback': onerror
+              });
+            }
+          };
+        } else if (config.provider === 'turnstile' && config.clientKey.length > 0) {
+          c = {
+            enabled: true,
+            scriptUrl: 'https://challenges.cloudflare.com/turnstile/v0/api.js?onload=captcha_onloadCallback',
+            render: (elementId, onsuccess, onexpired, onerror) => {
+              (window as any).turnstile.render('#' + elementId, {
+                'sitekey' : config.clientKey,
+                'callback': onsuccess,
+                'expired-callback': onexpired,
+                'error-callback': onerror
+              });
+            }
+          }
+        } else {
+          c = {
+            enabled: false,
+            scriptUrl: '',
+            render: (elementId, onsuccess) => {
+              Console.info('Captcha is disabled');
+              onsuccess('disabled');
+            },
+          }
+        }
+        this.config.next(c);
+        return c;
+      })
+    );
   }
 
 }
