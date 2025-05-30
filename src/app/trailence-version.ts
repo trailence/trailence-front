5d5d400f51c2af8b4a7916fce192c097d9ae8096
Guillaume Le Cousin <guillaume@lecousin.net>
2025-05-30 06:06:58 +0200

    v0.16.1

--- src/app/trailence-version.ts
+++ src/app/trailence-version.ts
@@ -1,2 +1,2 @@
-export const trailenceAppVersionName="0.16.0";
-export const trailenceAppVersionCode=1600;
+export const trailenceAppVersionName="0.16.1";
+export const trailenceAppVersionCode=1601;
