81fa2331626d255e5703a01d1ab25d647971675b
Guillaume Le Cousin <guillaume@lecousin.net>
2025-06-06 13:43:17 +0200

    v0.16.2

--- src/app/services/i18n/i18n.service.ts
+++ src/app/services/i18n/i18n.service.ts
@@ -7,7 +7,7 @@ import { StringUtils } from 'src/app/utils/string-utils';
 import { AssetsService } from '../assets/assets.service';
 import { Console } from 'src/app/utils/console';
 
-const TEXTS_VERSION = '20';
+const TEXTS_VERSION = '21';
 
 interface TextToLoad {
   filePath: string;
--- src/app/trailence-version.ts
+++ src/app/trailence-version.ts
@@ -1,2 +1,2 @@
-export const trailenceAppVersionName="0.16.1";
-export const trailenceAppVersionCode=1601;
+export const trailenceAppVersionName="0.16.2";
+export const trailenceAppVersionCode=1602;
