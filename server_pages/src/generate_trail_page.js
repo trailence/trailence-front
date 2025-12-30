09d52ad3bdf3f019eb98cbabac922aba9e284dd7
Guillaume Le Cousin <guillaume@lecousin.net>
2025-12-30 06:17:01 +0100

    v1.3.1

--- server_pages/src/generate_trail_page.js
+++ server_pages/src/generate_trail_page.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-const TEXTS_VERSION = '55';
+const TEXTS_VERSION = '56';
 
 async function generate(request) {
   if (request.method != 'GET') {
--- src/app/trailence-version.ts
+++ src/app/trailence-version.ts
@@ -1,2 +1,2 @@
-export const trailenceAppVersionName="1.3.0";
-export const trailenceAppVersionCode=10300;
+export const trailenceAppVersionName="1.3.1";
+export const trailenceAppVersionCode=10301;
