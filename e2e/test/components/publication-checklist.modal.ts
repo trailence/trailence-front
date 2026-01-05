01696cc3fd7796b0253bd648f57479c8bf6f7758
Guillaume Le Cousin <guillaume@lecousin.net>
2026-01-05 06:37:17 +0100

    fix test publication

--- e2e/test/components/publication-checklist.modal.ts
+++ e2e/test/components/publication-checklist.modal.ts
@@ -1,16 +1,18 @@
+import { TestUtils } from '../utils/test-utils';
 import { IonicCheckbox } from './ionic/ion-checkbox';
 import { ModalComponent } from './modal';
 
 export class PublicationChecklistModal extends ModalComponent {
 
   public async checkAll() {
-    for (let i = 0; i < 2; ++i) {
+    await TestUtils.retry(async () => {
       const checkboxes = this.getElement().$$('>>>ion-checkbox');
       const nb = await checkboxes.length
       for (let i = 0; i < nb; ++i) {
         await new IonicCheckbox(checkboxes[i]).setSelected(true);
       }
-    }
+      for (let i = 0; i < nb; ++i) if (await new IonicCheckbox(checkboxes[i]).getStatus() !== true) throw new Error('Checkbox still not checked');
+    }, 10, 250);
   }
 
 }
