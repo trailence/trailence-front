feb55b3f1f3c11a2ea232b618f7d76c1deee9207
Guillaume Le Cousin <guillaume@lecousin.net>
2026-07-10 05:27:01 +0200

    add build beta apk

---
+++ capacitor/environments/android.beta.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  serviceWorker: false,
+  apiBaseUrl: 'https://trailence.org/api',
+  assetsUrl: '/assets',
+  baseUrl: 'https://trailence.org',
+};
